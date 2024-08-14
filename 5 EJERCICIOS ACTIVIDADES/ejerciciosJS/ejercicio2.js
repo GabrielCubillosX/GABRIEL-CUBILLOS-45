179fcb3412400c9420c259ae870ce5855dd2bff0
GabrielCubillosX <gabrielestebancubillos@gmail.com>
2024-08-14 17:24:45 -0500

    instructor he realizado la mayor parte del algoritmo que pide el ejercicio 2 ya solo me falta agregarle la tabla y los estilos, poco a poco voy aprendiendo mas aun me falta mucho pero he sentido el progreso una vez acabe la funcionalidad de los primeros 3 ejercicios me dedicare a el diseño de las interfaces

--- 5 EJERCICIOS ACTIVIDADES/ejerciciosJS/ejercicio2.js
+++ 5 EJERCICIOS ACTIVIDADES/ejerciciosJS/ejercicio2.js
@@ -1,20 +1,84 @@
-function obtencionDatos (){
+const cedulaInput= document.getElementById("cedula")
+const cedulaInput2= document.getElementById("cedula2")
+const nombreInput= document.getElementById("nombre")
+const telefonoInput= document.getElementById("telefono")
+const edadInput= document.getElementById("edad")
+const formRegistro= document.getElementById("formularioRegistro")
+const titulo= document.getElementById("titulo")
+const titulo2= document.getElementById("titulo2")
+const form = document.getElementById("form")
+const boton= document.getElementById("botonSubmit")
+const botonRegistro = document.getElementById("botonRegistro")
+let registrados= [ ]
+
+function obtencionDatos (event){
+  event.preventDefault()
+if (!cedulaInput.value){
+  mostrarFormulario()
+}else{
+  busqueda(cedulaInput)
+  
+}
+}
 
 
+function mostrarFormulario(){
+ 
+  if (formRegistro.style.display === "none"){
+    form.style.display="none"
+    formRegistro.style.display="block"
+    titulo.innerHTML="REGíSTRESE EN EL SISTEMA"
+  } else {
+    formRegistro.style.display="none"
+    form.style.display="block"
+  }
+
+}
+
+function registrarUsuario(event) {
+  event.preventDefault()
+if (!nombreInput.value.trim() || !cedulaInput2.value.trim()|| !telefonoInput.value.trim()|| !edadInput.value.trim()) {
+alert("Llene todos los campos")
+}else{
+  let persona= {
+    nombre:nombreInput.value,
+    cedula:cedulaInput2.value,
+    telefono:telefonoInput.value,
+    edad:parseInt(edadInput.value)
+  }
+registrados.push(persona)
+console.log(registrados)
+guardarDatos()
+titulo2.innerHTML="REGISTRO EXITOSO"
+}
 }
 
 
 
+function busqueda(cedulaInpt) {
+  const resultado = registrados.find(i => i.cedula === cedulaInpt.value)
+if (resultado){
+alert("persona encontrada")
+}else {
+mostrarFormulario()
+}
+}
+
+
 function cargarDatos() {
-    const datos = localStorage.getItem('productosGuardados')
+  formRegistro.style.display = "none"
+    const datos = localStorage.getItem('personasGuardadas')
     if (datos) {
-        arrayTabla = JSON.parse(datos) /* CONVIERTE LOS DATOS JSON A UN ARRAY YA ACTUALIZA LA TABLA DE UNA VEZ */
-        actualizarDatos()
+        registrados = JSON.parse(datos) //CONVIERTE LOS DATOS JSON A UN ARRAY YA ACTUALIZA LA TABLA DE UNA VEZ
+        // actualizarDatos()
+        console.log(registrados)
     }
   }
   
   function guardarDatos() {
-    localStorage.setItem('productosGuardados', JSON.stringify(arrayTabla)) /* LOS ARRAY SE CONVIERTEN EN JSON PARA GUARDARLOS EN STORAGE */
+    localStorage.setItem('personasGuardadas', JSON.stringify(registrados)) //LOS ARRAY SE CONVIERTEN EN JSON PARA GUARDARLOS EN STORAGE
   }         
-  agregarbtn.addEventListener("click", searchselect)
+  
 window.onload = cargarDatos
+boton.addEventListener("click",obtencionDatos)
+botonRegistro.addEventListener("click",registrarUsuario)
